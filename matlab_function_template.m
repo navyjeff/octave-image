02525ab3deffad7d18290972af06cb9990e4f6bb
Jeffrey Borck <navyjeff@gmail.com>
2015-03-30 17:04:57 -0400

    Added template
    Added a template file to base functions on.

---
+++ matlab_function_template.m
@@ -0,0 +1,38 @@
+function [output1,output2] = function_name(input1,input2,input3)
+%function_name - One line description of what the function or script performs (H1 line)
+%Optional file header info (to give more details about the function)
+%Optional file header info (to give more details about the function)
+%Optional file header info (to give more details about the function)
+%
+% Syntax:  [output1,output2] = function_name(input1,input2,input3)
+%
+% Inputs:
+%    input1 - Description
+%    input2 - Description
+%    input3 - Description
+%
+% Outputs:
+%    output1 - Description
+%    output2 - Description
+%
+% Example: 
+%    Line 1 of example
+%    Line 2 of example
+%    Line 3 of example
+%
+% Other m-files required: none
+% Subfunctions: none
+% MAT-files required: none
+%
+% See also: other_function1, other_function2
+%
+% Author: Jeffrey Borck
+% 
+% email: navyjeff+m@gmail.com
+% Website: http://github.com/navyjeff
+% March 2015; Last revision: 30 Mar 2015
+
+%------------- Begin code  --------------
+
+
+%------------- End code    --------------
